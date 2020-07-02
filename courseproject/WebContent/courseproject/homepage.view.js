ab76aad1d6a89b0424a6aa247b933335dbe42684
Gayathri <gtalasila@innominds.com>
2020-07-02 14:06:10 +0530

    Second Commit
    added text to the button

--- courseproject/WebContent/courseproject/homepage.view.js
+++ courseproject/WebContent/courseproject/homepage.view.js
@@ -3,34 +3,40 @@ sap.ui.jsview("courseproject.homepage", {
 	/** Specifies the Controller belonging to this View. 
 	* In the case that it is not implemented, or that "null" is returned, this View does not have a Controller.
 	* @memberOf courseproject.homepage
-	*/ 
-	getControllerName : function() {
+	*/
+	getControllerName: function() {
 		return "courseproject.homepage";
 	},
 
 	/** Is initially called once after the Controller has been instantiated. It is the place where the UI is constructed. 
 	* Since the Controller is given to this method, its event handlers can be attached right away. 
 	* @memberOf courseproject.homepage
-	*/ 
-	createContent : function(oController) {
- 		var oTileTemp = new sap.m.StandardTile({
-			icon:"{icon}",
-			title:"{Subject}",
-			info:"{Type}"
+	*/
+	createContent: function(oController) {
+		var oTileTemp = new sap.m.StandardTile({
+			icon: "{icon}",
+			title: "{Subject}",
+			info: "{Type}"
 		})
-	
-	  
-		
+
+
+
 		var oTileCont = new sap.m.TileContainer({
-			
+
 		});
-		var oButton = new sap.m.Button({icon:"sap-icon://log", press: function(oEvent){app.back();}});
-		var oHeader = new sap.m.Bar({ contentRight:[oButton], contentMiddle:[new sap.m.Label({text:"Courses"})]});
-		oTileCont.bindAggregation("tiles","/name",oTileTemp);
- 		var oPage = new sap.m.Page({
+		var oButton = new sap.m.Button({
+			icon: "sap-icon://log",
+			press: function(oEvent) {
+				app.back();
+			},
+			text: "Logout"
+		});
+		var oHeader = new sap.m.Bar({ contentRight: [oButton], contentMiddle: [new sap.m.Label({ text: "Courses" })] });
+		oTileCont.bindAggregation("tiles", "/name", oTileTemp);
+		var oPage = new sap.m.Page({
 			content: [
 				oHeader,
-			    oTileCont
+				oTileCont
 			]
 		});
 		return oPage;
